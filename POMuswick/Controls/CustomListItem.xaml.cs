dcc4de485eccdbb18ffd40014627bad668fa69d6
estonsoft <estonsoftpvtltd@gmail.com>
2026-09-08 19:40:18 +0530

    Fixed Image scrolling issue

--- POMuswick/Controls/CustomListItem.xaml.cs
+++ POMuswick/Controls/CustomListItem.xaml.cs
@@ -9,6 +9,47 @@ namespace POMuswick.Controls
             InitializeComponent();
         }
 
+        private void Grid_Loaded(object sender, EventArgs e)
+        {
+            // 1. Get the data model from the cell
+            if (BindingContext is Item model && !string.IsNullOrEmpty(model.ImageURL))
+            {
+                // 2. Show spinner while loading
+                MySpinner.IsVisible = true;
+                MySpinner.IsRunning = true;
+
+                // 3. Set the image source explicitly
+                MyImage.Source = ImageSource.FromUri(new Uri(model.ImageURL));
+
+                // Hide spinner when done (or handle via event/async)
+                MySpinner.IsVisible = false;
+                MySpinner.IsRunning = false;
+            }
+        }
+
+        private void Grid_Unloaded(object sender, EventArgs e)
+        {
+            // 1. Stop the spinner immediately
+            MySpinner.IsRunning = false;
+            MySpinner.IsVisible = false;
+
+            // 2. Clear the image source to release memory when scrolling away
+            MyImage.Source = null;
+
+            // 3. Drop native platform references
+#if ANDROID
+        if (MyImage.Handler?.PlatformView is Android.Widget.ImageView nativeAndroid)
+        {
+            nativeAndroid.SetImageBitmap(null);
+        }
+#elif IOS
+            if (MyImage.Handler?.PlatformView is UIKit.UIImageView nativeIOS)
+            {
+                nativeIOS.Image = null;
+            }
+#endif
+        }
+
         private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
         {
             if (App.imageAlertView != null)
