24d70365521fedf65cd3e831383a48098b70a876
estonsoft <estonsoftpvtltd@gmail.com>
2026-09-09 18:05:14 +0530

    Fixed Image scrolling same image issue

--- POMuswick/Controls/CustomSmallListItem.xaml.cs
+++ POMuswick/Controls/CustomSmallListItem.xaml.cs
@@ -14,40 +14,14 @@ namespace POMuswick.Controls
             // 1. Get the data model from the cell
             if (BindingContext is Item model && !string.IsNullOrEmpty(model.ImageURL))
             {
-                // 2. Show spinner while loading
-                MySpinner.IsVisible = true;
-                MySpinner.IsRunning = true;
-
-                // 3. Set the image source explicitly
                 MyImage.Source = ImageSource.FromUri(new Uri(model.ImageURL));
-
-                // Hide spinner when done (or handle via event/async)
-                MySpinner.IsVisible = false;
-                MySpinner.IsRunning = false;
             }
+            MyImage.IsVisible = true;
         }
 
         private void Grid_Unloaded(object sender, EventArgs e)
         {
-            // 1. Stop the spinner immediately
-            MySpinner.IsRunning = false;
-            MySpinner.IsVisible = false;
-
-            // 2. Clear the image source to release memory when scrolling away
-            MyImage.Source = null;
-
-            // 3. Drop native platform references
-#if ANDROID
-        if (MyImage.Handler?.PlatformView is Android.Widget.ImageView nativeAndroid)
-        {
-            nativeAndroid.SetImageBitmap(null);
-        }
-#elif IOS
-            if (MyImage.Handler?.PlatformView is UIKit.UIImageView nativeIOS)
-            {
-                nativeIOS.Image = null;
-            }
-#endif
+            MyImage.IsVisible = false;
         }
 
         private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
